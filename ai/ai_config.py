4ee5eb03e2c83850c248518db881357e932136aa
dzobamain <knopkayt26@gmail.com>
2025-04-27 16:25:17 +0200

    Corrected the title GIMINI_API_KEY

--- ai/ai_config.py
+++ ai/ai_config.py
@@ -10,7 +10,7 @@ def read_from_file(file_path):
 gemini_instruct_file_path = "ai/gemini_instruct.txt"
 gemini_instruct=read_from_file(gemini_instruct_file_path)
 gemini_model = "gemini-2.0-flash"
-gemini_api_key = os.getenv("GIMINEI_API_KEY")
+gemini_api_key = os.getenv("GIMINI_API_KEY")
 
 if not gemini_api_key:
     raise ValueError("API key not found! Please check your .env file.")
